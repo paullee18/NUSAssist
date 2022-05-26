38de47e29cf3bf7e6fa6bea35198a49ceeb97608
Paul Lee <paulfrancislee.1@gmail.com>
2022-05-26 14:19:05 +0800

    Used Material UI to route pages and style NavBar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css';
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Navigation from "./components/Navigation"
-import Calendar from "./components/Calendar"
-import Footer  from "./components/Footer"
-import TaskManager from "./components/TaskManager"
-import Home from "./components/Home"
-
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/task-manager" element={<TaskManager />} />
-        </Routes>
-        <Footer />
-      </Router>
-
-
-    </div>
+    
+      <div>
+      </div>
   );
 }
 
