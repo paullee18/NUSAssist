787f59ca9a1743aab8c83cbfab8d594d7f144b69
T Neethesh <neethesh26@gmail.com>
2022-07-25 01:10:22 +0800

    Update the calendar so that form is viewable

--- client/src/components/calendar/components/EventModal.js
+++ client/src/components/calendar/components/EventModal.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import GlobalContext from "../context/GlobalContext";
+import Stack from '@mui/material/Stack';
 
 // const labelsClasses = [
 //   "blue",
@@ -55,25 +56,11 @@ export default function EventModal() {
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4" onSubmit={handleSubmit}>
-        <header className="bg-gray-100 px-4 py-2 flex justify-between items-center">
+        <header className="bg-gray-100 px-3 py-2 flex justify-between items-center">
           <span className="material-icons-outlined text-gray-400">
             drag_handle
           </span>
-          <div>
-            {selectedEvent && (
-              <span
-                onClick={() => {
-                  dispatchCalEvent({
-                    type: "delete",
-                    payload: selectedEvent,
-                  });
-                  setShowEventModal(false);
-                }}
-                className="material-icons-outlined text-gray-400 cursor-pointer"
-              >
-                delete
-              </span>
-            )}
+          <div> 
             <button onClick={() => setShowEventModal(false)}>
               <span className="material-icons-outlined text-gray-400">
                 close
@@ -81,9 +68,8 @@ export default function EventModal() {
             </button>
           </div>
         </header>
-        <div className="p-3">
+        <div className="px-3 py-1">
           <div className="grid grid-cols-1/5 items-end gap-y-7">
-            <div></div>
             <label htmlFor="Title" className="my-2 font-link" style={{fontWeight: 'bold'}}>
                  Enter Title : 
                </label>
@@ -117,6 +103,7 @@ export default function EventModal() {
               type="time"
               name="begin"
               value={begin}
+              required
               className="w-100 my-1 p-2 font-link"
               onChange={(e) => setBegin(e.target.value)}
             />
@@ -129,27 +116,27 @@ export default function EventModal() {
               type="time"
               name="end"
               value={end}
+              required
               className="w-100 my-1 p-2 font-link"
               onChange={(e) => setEnd(e.target.value)}
             />
-            {/* <div className="flex gap-x-2">
-              {labelsClasses.map((lblClass, i) => (
-                <span
-                  key={i}
-                  onClick={() => setSelectedLabel(lblClass)}
-                  className={`bg-blue-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}
-                >
-                  {selectedLabel === lblClass && (
-                    <span className="material-icons-outlined text-white text-sm">
-                      check
-                    </span>
-                  )}
-                </span>
-              ))}
-            </div> */}
           </div>
         </div>
-        <footer className="flex justify-end border-t p-3 mt-5">
+        <footer className="flex justify-between items-center border-t p-3 mt-5">
+        {selectedEvent && (
+              <button
+                onClick={() => {
+                  dispatchCalEvent({
+                    type: "delete",
+                    payload: selectedEvent,
+                  });
+                  setShowEventModal(false);
+                }}
+                className="bg-red-500 hover:bg-red-600 px-6 py-2 rounded text-white"
+              >
+                Delete    
+              </button>
+            )}
           <button
             type="submit"
             className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded text-white"
