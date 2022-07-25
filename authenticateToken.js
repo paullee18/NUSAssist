168231e3e014fe9da450218accdfacf7ef9d0a33
Paul Lee <paulfrancislee.1@gmail.com>
2022-07-25 21:26:55 +0800

    used Heroku config vars

--- authenticateToken.js
+++ authenticateToken.js
@@ -1,5 +1,17 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json');
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": "nusassist",
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": "firebase-adminsdk-vg0am@nusassist.iam.gserviceaccount.com",
+  "client_id": "102615693358193064986",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-vg0am%40nusassist.iam.gserviceaccount.com"
+
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
---
+++ index.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = require('./routes/api/tasks');
+const eventsRouter = require('./routes/api/events');
+const db = require('./models/task');
+const eventsDb = require('./models/event');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const path = require('path');
+
+mongoose.connect(
+    process.env.mongoDb,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('Connected to database');
+        })
+        .catch((err) => {
+            console.log('Error connecting to DB', err.message);
+        });
+    
+
+const app = express();
+
+const decodeIDToken = require('./authenticateToken');
+app.use(decodeIDToken);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')))
+// // Anything that doesn't match the above, send back index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
+
+// tasks API routes
+app.use('/api/tasks', router);
+
+// events API routes
+app.use('/api/events', eventsRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
